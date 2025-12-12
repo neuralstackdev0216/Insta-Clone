f73bc27d7f97e07c90705c42bb7c335a893d28c4
neuralstackdev0216 <neuralstack.dev@gmail.com>
2025-12-13 02:18:40 +0530

    Story Beginning

--- src/Stories.jsx
+++ src/Stories.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Stories() {
+
+  const [Stories,setStories]=useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:3000/story')
+    .then(data=>data.json())
+    .then(data=>setStories(data))
+    .catch(err=>console.log(err))
+  },[]);
+
   return (
-    <div className='story bg-info'>Stories</div>
-  )
+    <>
+    <div className='story d-flex'>
+      {Stories.length >0 ? (
+        Stories.map((story)=>(
+          <div key={story.id} className='mx-1'>
+            <div className='gradient-border'>
+              <img src={story.user.profile_pic} alt="dp" className='story-dp rounded-circle '/>
+            </div>
+            
+            <p className='text-truncate' style={{width:"50px"}}>{story.user.username}</p>
+          </div>
+      ))
+      ):(
+        <p>Loading</p>
+      )}
+    </div>
+    </>
+    
+  );
 }
 
 export default Stories
